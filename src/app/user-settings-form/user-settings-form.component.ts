9113deaf8a216d4ee3423d5e98593c4973038b3a
Edgar <edgar@pop-os.localdomain>
2019-05-09 01:17:25 -0500

    I don't want to write a commit message

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserSettingsFormComponent implements OnInit {
 
+  originalUserSettings: UserSettings = {
+    name: null,
+    emailOffers: null,
+    interfaceStyle: null,
+    subscriptionType: null,
+    notes: null
+  };
+
+  userSettings: UserSettings = { ...this.originalUserSettings };
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onBlur(field: NgModel) {
+    console.log("in onBlur: ", field.valid)
+  }
+
+  onSubmit(form: NgForm) {
+    console.log("in onSubmit: ", form.valid);
   }
 
 }
